9185179d3a9563f52456684e3c92bde2259d6ea8
Urias <GodofDrakes@gmail.com>
2022-01-20 14:42:37 -0800

    Initial project

---
+++ ECSThing.Desktop/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ECSThing.Desktop
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new ECSGame())
+                game.Run();
+        }
+    }
+}
